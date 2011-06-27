7817dc3f3488cd05de708364373c81760329e391
Fabricio Raphael Baliza <fabricioraphael.b@gmail.com>
2011-06-26 22:45:28 -0300

    commit #1

--- src/core/SubtitlesManager.java
+++ src/core/SubtitlesManager.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
@@ -31,9 +32,19 @@ public class SubtitlesManager {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
 		// dis.available() returns 0 if the file does not have more lines.
-		String line = "";
+		String line = "", time0 = "", time1 = "";
+		List<String> extract;
+		List<Date> date = null;
+		List<String> result;
 		while (line != null) {
 			line = reader.readLine();
+			try {
+				extract = this.extractTimes(line);
+				date = this.convertStringToTime(extract);
+				time0 = this.addOrSubTime(date.get(0), Operations.SUM, 2);
+				time1 = this.addOrSubTime(date.get(1), Operations.SUM, 2);
+				System.out.println("Dates: " + date + "   Time0: " + time0 + "   Time1: " + time1);
+			} catch (Exception e) {	}
 			text.append(line);
 		}
 
@@ -44,10 +55,16 @@ public class SubtitlesManager {
 		return text.toString();
 	}
 	
+	public void addText(String arq, String conteudo, boolean save) throws IOException{
+		FileWriter fw = new FileWriter(arq, save);
+
+		fw.write(conteudo);
+		fw.close();
+	}
+	
 	
 	public List<String> extractTimes(String text){
 		List<String> times = new ArrayList<String>();
-		
 		Pattern pattern = Pattern.compile("\\d{2}:\\d{2}:\\d{2}");
 		Matcher matcher = pattern.matcher(text);
 		
@@ -61,20 +78,40 @@ public class SubtitlesManager {
 	public List<Date> convertStringToTime(List<String> times) throws ParseException{
 		List<Date> dates = new ArrayList<Date>();
 		
-		for (String time : times) {
-			SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-			dates.add(format.parse(time));
-		}
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+		dates.add(format.parse(times.get(0)));
+		dates.add(format.parse(times.get(1)));
 		
 		return dates;
 	}
 	
-	public List<Date> addOrSubTime(List<Date> times, Operations operator, int amount){
-		List<Date> newDate = new ArrayList<Date>();
-		Calendar calendar = Calendar.getInstance();
+	public String sub(List<String> listString) throws IOException{
+		String leg;
 		
-		for (Date time : times) {
-			calendar.setTime(time);
+		leg = this.readFile("/Users/fabricioraphael/subtitle/tmplegenda.srt");
+		
+		
+		
+		
+		
+		
+		return null;
+	}
+	
+	public String clearDate(String date){
+		Pattern pattern = Pattern.compile("\\d{2}:\\d{2}:\\d{2}");
+		Matcher matcher = pattern.matcher(date);
+		
+		while(matcher.find()){
+			date = matcher.group();
+		}
+		return date;
+	}
+	
+	public String addOrSubTime(Date times, Operations operator, int amount){
+		String newDate;
+		Calendar calendar = Calendar.getInstance();
+			calendar.setTime(times);
 			
 			if (operator.equals(Operations.SUM)){
 				calendar.add(Calendar.SECOND, amount);
@@ -82,9 +119,8 @@ public class SubtitlesManager {
 				calendar.add(Calendar.SECOND, -amount);
 			}
 			
-			newDate.add(calendar.getTime());
-		}
-		
-		return newDate;
+			newDate = calendar.getTime().toString();
+			
+		return this.clearDate(newDate);
 	}
 }
---
+++ src/core/Upload.java
@@ -0,0 +1,68 @@
+package core;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Upload
+ */
+@WebServlet(name="/Upload", urlPatterns={"/upload.do"})
+public class Upload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Upload() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			SubtitlesManager subTitle = new SubtitlesManager();
+			String txt;
+			List<String> times;
+			List<Date> date = null;
+			List<Date> newDate = null;
+			List<String> finalDate = null;
+			
+			txt = subTitle.readFile("/Users/fabricioraphael/subtitle/tmplegenda.srt");
+			
+//			times = subTitle.extractTimes(txt);
+			
+//			System.out.println("Times extract:   " + times + "\n");
+//			
+//			try {
+//				date = subTitle.convertStringToTime(times);
+//			} catch (ParseException e) {
+//				System.out.println("Passou aqui catch!");
+//				e.printStackTrace();
+//			}
+			
+//			newDate = subTitle.addOrSubTime(date, Operations.SUM, 2);
+			
+//			System.out.println(newDate + "\n");
+			
+//			finalDate = subTitle.convertListToString(newDate); 
+			
+//			System.out.println(finalDate);
+			
+	}
+
+}
